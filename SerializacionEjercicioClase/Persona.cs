8a1f94c17481cb265ccd1cf10e43e46fc8679dd1
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-05-07 10:02:56 -0400

    casi lsito, solo arreglar errores

---
+++ SerializacionEjercicioClase/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+namespace SerializacionEjercicioClase
+{
+    [Serializable()]
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private List<Persona> listapersona = new List<Persona>();
+        public Persona(string nombre, string apellido, int edad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+        public void crearpersona(string nombre, string apellido, int edad)
+        {
+            Persona persona = new Persona(nombre, apellido, edad);
+            listapersona.Add(persona);
+        }
+        public void verpersonasregistrada()
+        {
+            foreach (Persona p in listapersona)
+            {
+                p.GetInfo();
+            }
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Nombre: {nombre}, apellido: {apellido}, edad: {edad}");
+        }
+    }
+}
--- SerializacionEjercicioClase/Program.cs
+++ SerializacionEjercicioClase/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 namespace SerializacionEjercicioClase
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona persona = new Persona("", "", 1);
+            bool exec = true;
+            while (exec)
+            {
+                string chosen = ShowOptions(new List<string>() { "Crear persona", "Ver personas registradas", "Almacenar", "Cargar" });
+                switch (chosen)
+                {
+                    case "Crear persona":
+                        Console.WriteLine("Cual seria su nombre?");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Cual seria su apellido?");
+                        string apellido = Console.ReadLine();
+                        Console.WriteLine("Cual seria su edad?");
+                        int edad = Convert.ToInt32(Console.ReadLine());
+                        persona.crearpersona(nombre, apellido, edad);
+                        Console.Clear();
+                        break;
+                    case "Ver personas registradas":
+                        Console.Clear();
+                        persona.verpersonasregistrada();
+                        break;
+                    case "Almacenar":
+                        Console.Clear();
+                        IFormatter formatter = new BinaryFormatter();
+                        Stream stream = new FileStream("ArchivoPersona.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                        formatter.Serialize(stream, persona);
+                        stream.Close();
+                        break;
+                    case "Cargar":
+                        Console.Clear();
+                        IFormatter formatter = new BinaryFormatter();
+                        Stream stream = new FileStream("ArchivoPersona.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                        Persona persona = (Persona)formatter.Deserialize(stream);
+                        stream.Close();
+                        break;
+                    case "Salir":
+                        exec = false;
+                        break;
+                }
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+        }
+        private static string ShowOptions(List<string> options)
+        {
+            int i = 0;
+            Console.WriteLine("\n\nSelecciona una opcion:");
+            foreach (string option in options)
+            {
+                Console.WriteLine(Convert.ToString(i) + ". " + option);
+                i += 1;
+            }
+            return options[Convert.ToInt16(Console.ReadLine())];
         }
     }
 }
