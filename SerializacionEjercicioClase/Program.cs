2b0897713558bc8ece068fd309979fafe54350c5
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-05-07 09:00:34 -0400

    listo el gitignore

---
+++ SerializacionEjercicioClase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializacionEjercicioClase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
